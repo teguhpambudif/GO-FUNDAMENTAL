a5b290d95753cea1ac9a2428074325de10e343be
teguhism <tegguh1251@gmail.com>
2022-05-25 23:13:26 +0700

    operation

---
+++ teguh-learn-golang/CHAPTER-A-FUNDAMENTAL/A-12-Operator/12-3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var left = false
+	var right = true
+
+	var leftAndRight = left && right
+	fmt.Printf("left && right \t(%t) \n", leftAndRight)
+
+	var leftOrRight = left || right
+	fmt.Printf("left || right \t(%t) \n", leftOrRight)
+
+	var leftReverse = !left
+	fmt.Printf("!left \t\t(%t) \n", leftReverse)
+
+}
