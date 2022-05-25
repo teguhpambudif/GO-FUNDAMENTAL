ea081b8281bd704a4e1644d6e9af57a9c98379aa
teguhism <tegguh1251@gmail.com>
2022-05-26 00:19:26 +0700

    if-else-go

---
+++ teguh-learn-golang/CHAPTER-A-FUNDAMENTAL/A-13-if-else-condition/13-5.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var point = 2
+
+	switch point {
+	case 8:
+		fmt.Println("perfect")
+	case 7, 6, 5, 4:
+		fmt.Println("awesome")
+	default:
+		{
+			fmt.Println("not bad")
+			fmt.Println("you can be better!")
+		}
+	}
+
+}
