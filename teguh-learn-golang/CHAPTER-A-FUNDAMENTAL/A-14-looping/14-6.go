52d6e70ed64d31198a8f0244326995b941bed5ca
teguhism <tegguh1251@gmail.com>
2022-05-28 03:32:18 +0700

    looping on go

---
+++ teguh-learn-golang/CHAPTER-A-FUNDAMENTAL/A-14-looping/14-6.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// labelling in loop
+outerLoop:
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if i == 3 {
+				break outerLoop
+			}
+			fmt.Print("matriks [", i, "][", j, "]", "\n")
+		}
+	}
+}
