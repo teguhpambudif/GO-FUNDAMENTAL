76fd6d4eb8cf7dc75535e497dc512808c9d2e821
teguhpambudif <teguhpambudi1251@gmail.com>
2022-12-20 22:41:49 +0700

    baca isi& hapus file

---
+++ A-50-file/3-membaca-isi.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var path = "/home/teguhpambudif/Downloads/temp/test.txt"
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return (err != nil)
+}
+
+func readFile() {
+	// buka file
+	var file, err = os.OpenFile(path, os.O_RDONLY, 0644)
+	if isError(err) {
+		return
+	}
+	defer file.Close()
+
+	// baca file
+	var text = make([]byte, 1024)
+	for {
+		n, err := file.Read(text)
+		if err != io.EOF {
+			if isError(err) {
+				break
+			}
+		}
+		if n == 0 {
+			break
+		}
+	}
+	if isError(err) {
+		return
+	}
+
+	fmt.Println("==> file berhasil dibaca")
+	fmt.Println(string(text))
+}
+
+func main() {
+	readFile()
+}
